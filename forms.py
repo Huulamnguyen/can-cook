267389fd37d552c88a9a3d05234aa5e3c8546eb9
Huu Lam Nguyen <liam.swe@Huus-MBP.home>
2021-03-04 22:36:52 -0500

    setting up forms

---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length
+
+class UserForm(FlaskForm):
+    """ Form for adding/editing users. """
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+    image_url = StringField('(Optional) Image URL')
+
+class LoginForm(FlaskForm):
+    """Login form."""
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
+
