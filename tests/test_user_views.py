762bfee6d4ae8a1e073e832e8a53a2aa9d83684c
Huu Lam Nguyen <liam.swe@Huus-MacBook-Pro.local>
2021-07-09 14:04:10 -0400

    building recipe deatil page

--- tests/test_user_views.py
+++ tests/test_user_views.py
@@ -7,28 +7,36 @@ class UserViewTestCase(TestCase):
 
     def setUp(self):
         """Create test client"""
-
         self.app = create_app('testing')
         self.client = self.app.test_client()
 
     def tearDown(self):
         response = super().tearDown()
         return response
+
     # Test Homepage
     def test_homepage(self):
         """Test homepage"""
         with self.client as c:
             response = c.get('/')
             html = response.get_data(as_text=True)
-
             self.assertEqual(response.status_code, 200)
             self.assertIn('Homepage', html)
+
     # Test Search Recipes
     def test_search_recipes(self):
         """ Test Recipes page"""
         with self.client as c:
             response = c.get('/recipes')
             html = response.get_data(as_text=True)
-
             self.assertEqual(response.status_code, 200)
             self.assertIn('Recipes', html)
+
+    def test_recipe_details(self):
+        """ Test Recipe Deatail Page"""
+        with self.client as c:
+            response = c.get("/recipes/642373")
+            html = response.get_data(as_text=True)
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('Recipe Detail', html)
+            self.assertIn('Elk Sliders With Pancetta Bacon and Smoked Mozzarella', html)
